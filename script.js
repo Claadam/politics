6231fb9df35b9baefc5ba3026bf1f22ecffa8644
WalnutsAreCool <claadam@nuevaschool.org>
2016-12-06 08:53:14 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
+var display= $("#display")
 $.ajax({
   method: "GET",
   url: "https://www.govtrack.us/api/v2/person",
   success: function(data) {    
     data.objects.forEach(function(object) {
       var arr = []
-      arr.push(object.firstname)
-      $("#id").html(arr)
-//      object.firstname.append($("#id"))
-
+      object.firstname().append(display)
     })
   }
 })
